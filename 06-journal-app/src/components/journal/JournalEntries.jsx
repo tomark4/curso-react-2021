fb1bc1cd4f7dcbbf9a5b84afa9d2b8c66d63dc42
Jose Quintero <jg.fernandez31@gmail.com>
2021-03-31 18:38:03 -0400

    diseño journal app finalizado

---
+++ 06-journal-app/src/components/journal/JournalEntries.jsx
@@ -0,0 +1,20 @@
+import JournalEntry from './JournalEntry';
+
+const JournalEntries = () => {
+    const entries = [1,2,3,4,5, 1,2,3,4,5 ,1,2,3,4,5 ];
+
+
+    return (
+        <div className="journal__entries">
+
+            {
+                entries.map( value => (
+                    <JournalEntry key={ value } />
+                ))
+            }
+
+        </div>
+    )
+}
+
+export default JournalEntries
---
+++ 06-journal-app/src/components/journal/NothingSelected.jsx
@@ -0,0 +1,18 @@
+
+const NothingSelected = () => {
+
+    return (
+        <div className="nothing__main-content">
+            <p>
+                Select something
+                <br />
+                pr create an entry!
+            </p>
+
+            <i className="far fa-star fa-4x mt-5"></i>
+
+        </div>
+    )
+}
+
+export default NothingSelected
