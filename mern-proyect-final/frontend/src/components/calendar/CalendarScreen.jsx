f96175fe139c5e606ad17aa66d1031c392922a54
Jose Quintero <jg.fernandez31@gmail.com>
2021-04-09 15:41:05 -0400

    configure big calendar

--- mern-proyect-final/frontend/src/components/calendar/CalendarScreen.jsx
+++ mern-proyect-final/frontend/src/components/calendar/CalendarScreen.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import Navbar from '../ui/Navbar'
+import {Calendar, momentLocalizer} from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const localizer = momentLocalizer(moment);
+
+const events = [
+    {
+        title:'lorem',
+        start: moment().toDate(),
+        end: moment().add(2,'hours').toDate(),
+        bgcolor:'#fafafa'
+    }
+]
 
 const CalendarScreen = () => {
     return (
-        <div>
+        <div className="calendar-screen">
             <Navbar />
+            <Calendar
+            localizer={localizer}
+            events={events}
+            startAccesor="start"
+            endAccessor="end"/>
         </div>
     )
 }
