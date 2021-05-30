48cc4c43005bb754e20f462a441859aa7b380475
Jose Quintero <jg.fernandez31@gmail.com>
2021-05-30 15:56:10 -0400

    install cors

--- mern-proyect-final/backend/index.js
+++ mern-proyect-final/backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { dbConnection } = require("./database/config");
 require('dotenv').config();
+const cors = require("cors");
 
 const app = express();
 
@@ -9,6 +10,7 @@ dbConnection();
 // public dir
 app.use(express.static('public'));
 app.use(express.json())
+app.use(cors());
 
 // Routes
 app.use("/api/auth",require('./routes/auth'));
